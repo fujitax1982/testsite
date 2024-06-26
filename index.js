7722e512671f2f3b1c8f2d238e2669dfee861b75
fujitax <fujitax@gmail.com>
2024-06-26 10:41:55 +0900

    Update index.js

--- index.js
+++ index.js
@@ -4,28 +4,34 @@ const cameraHeight = 400;
 const cameraInitSmartphoneSupport = async () => {
     const video = document.getElementById("camera");
 
-    // 利用可能なカメラデバイスのリストを取得
-    const devices = await navigator.mediaDevices.enumerateDevices();
-    const videoDevices = devices.filter(device => device.kind === 'videoinput');
-
-    // 前面カメラを見つける
-    let frontCamera = videoDevices.find(device => device.label.toLowerCase().includes('front')) || videoDevices[0];
-
-    const cameraSetting = {
-        audio: false,
-        video: {
-            deviceId: { exact: frontCamera.deviceId },
-            width: cameraWidth,
-            height: cameraHeight,
-            facingMode: "user"
-        }
-    };
-
-    // カメラを起動
     try {
+        // 利用可能なカメラデバイスのリストを取得
+        const devices = await navigator.mediaDevices.enumerateDevices();
+        const videoDevices = devices.filter(device => device.kind === 'videoinput');
+
+        // 前面カメラを見つける
+        let frontCamera = videoDevices.find(device => device.label.toLowerCase().includes('front')) || videoDevices[0];
+
+        if (!frontCamera) {
+            console.error('Front camera not found');
+            return;
+        }
+
+        const cameraSetting = {
+            audio: false,
+            video: {
+                deviceId: { exact: frontCamera.deviceId },
+                width: cameraWidth,
+                height: cameraHeight,
+                facingMode: "user"
+            }
+        };
+
+        // カメラを起動
         const mediaStream = await navigator.mediaDevices.getUserMedia(cameraSetting);
         video.srcObject = mediaStream;
     } catch (err) {
         console.error('Error accessing the camera: ', err);
+        alert('カメラにアクセスできませんでした。ブラウザの設定を確認してください。');
     }
 }
