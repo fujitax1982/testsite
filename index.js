50b31f13bded9277fc02941421df4cc2b58919f3
fujitax <fujitax@gmail.com>
2024-06-26 11:59:09 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
 const cameraWidth = 640;
 const cameraHeight = 480;
 
+const requestPermissions = async () => {
+    try {
+        // カメラとマイクへのアクセスをリクエスト
+        await navigator.mediaDevices.getUserMedia({ video: true, audio: true });
+        console.log('Permissions granted');
+    } catch (err) {
+        console.error('Permission request failed:', err);
+    }
+};
+        
 const cameraInitSmartphoneSupport = async () => {
     const video = document.getElementById("camera");
 
+    // パーミッションをリクエスト
+    await requestPermissions();
+    
     // 利用可能なカメラデバイスのリストを取得
     const devices = await navigator.mediaDevices.enumerateDevices();
     console.log('Devices:', devices);
